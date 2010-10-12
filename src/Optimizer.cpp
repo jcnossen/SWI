672e3b05c8ab879aaf04d3a99524ddfff35cc0d5
jcnossen <jcnossen@nospam.com>
2010-10-12 13:35:41 +0200

    * Speed up compilation * Optimizer definition

---
+++ src/Optimizer.cpp
@@ -0,0 +1,18 @@
+#include "StdIncl.h"
+#include "Optimizer.h"
+
+SwarmOptimizer::SwarmOptimizer()
+{
+}
+
+void SwarmOptimizer::initialize(int ndims, int nelems)
+{
+}
+
+void SwarmOptimizer::tick(float* fitness)
+{
+}
+
+void SwarmOptimizer::getElem(float* params)
+{
+}
--- src/Optimizer.h
+++ src/Optimizer.h
@@ -1,8 +1,27 @@
 #pragma once
 
-
 class IOptimizer
 {
 public:
+	virtual void initialize(int ndims, int nelems) {}
+	virtual const char* name() = 0;
+	virtual void tick(float* fitness) = 0; // taking fitness[nelems]
+	virtual void getElem(float* params) = 0;
 };
 
+class SwarmOptimizer : public IOptimizer
+{
+public:
+	SwarmOptimizer();
+
+	const char* name() { return "PSO"; }
+	void initialize(int ndims, int nelems);
+	void tick(float* fitness);
+	void getElem(float* params);
+};
+
+
+class GAOptimizer : public IOptimizer
+{
+	const char* name() { return "GA"; }
+};
